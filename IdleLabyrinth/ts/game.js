e9614822e8594e6468d3cd13dff03052f076bf29
MaxKlaxxMiner <max@tetrisk.de>
2020-06-15 20:40:13 +0200

    add canvas

--- IdleLabyrinth/ts/game.js
+++ IdleLabyrinth/ts/game.js
@@ -1,15 +1,24 @@
 /* tslint:disable:one-line max-line-length interface-name comment-format */
 var Game = (function () {
-    function Game() {
-    }
-    Game.test = function (gameDiv) {
+    function Game(gameDiv) {
+        this.gameDiv = gameDiv;
         gameDiv.style.width = "1280px";
         gameDiv.style.height = "720px";
         gameDiv.style.backgroundColor = "#036";
+        var canvas = document.createElement("canvas");
+        canvas.width = 1280;
+        canvas.height = 720;
+        this.ctx = canvas.getContext("2d");
+        gameDiv.appendChild(canvas);
+    }
+    Game.prototype.test = function () {
+        var g = this.ctx;
+        g.fillRect(100, 100, 1280 - 200, 720 - 200);
     };
     return Game;
 })();
 window.onload = function () {
-    var div = document.getElementById("game");
-    Game.test(div);
+    var game = new Game(document.getElementById("game"));
+    game.test();
 };
+//# sourceMappingURL=game.js.map
--- IdleLabyrinth/ts/game.ts
+++ IdleLabyrinth/ts/game.ts
@@ -1,17 +1,33 @@
-﻿ /* tslint:disable:one-line max-line-length interface-name comment-format */
+﻿/* tslint:disable:one-line max-line-length interface-name comment-format */
 
 class Game
 {
-  static test(gameDiv: HTMLElement): void
+  gameDiv: HTMLElement;
+  ctx: CanvasRenderingContext2D;
+
+  constructor(gameDiv: HTMLElement)
   {
+    this.gameDiv = gameDiv;
     gameDiv.style.width = "1280px";
     gameDiv.style.height = "720px";
     gameDiv.style.backgroundColor = "#036";
+
+    var canvas = document.createElement("canvas");
+    canvas.width = 1280;
+    canvas.height = 720;
+    this.ctx = canvas.getContext("2d");
+    gameDiv.appendChild(canvas);
+  }
+
+  test(): void
+  {
+    var g = this.ctx;
+    g.fillRect(100, 100, 1280 - 200, 720 - 200);
   }
 }
 
 window.onload = () =>
 {
-  var div = document.getElementById("game");
-  Game.test(div);
+  var game = new Game(document.getElementById("game"));
+  game.test();
 }
